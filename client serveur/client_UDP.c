acee7d6827a5746ca06969ec628ca6c0569f4f56
edulorandi <edulorandi>
2020-02-14 10:27:59 +0100

    Making a header for shared structures and includes, changing the server and the client to stop doing bullshit and adding a compile bash

--- client serveur/client_UDP.c
+++ client serveur/client_UDP.c
@@ -1,61 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <fcntl.h>
+#include "utils.h"
 
-struct mesg {
+const char* STD_ADDR = "127.0.0.1";
 
-double label;
-double position[1];
-double control[1];
+int main (int argc, char *arg[])
+{
+	int result;
+	int nsend;
+	int nconnect;
+	struct mesg message;
+	int addr;
+	long int Te;
 
-};
-
-#define ERROR (-1)
-
-int main (int nba, char *arg[]) {
-int result;
-int nsend;
-int nconnect;
-struct mesg message;
-int addr;
-long int Te;
-
-struct sockaddr_in sockAddr, sock;
-int serveur, client, err, nConnect, longaddr , results, resultr;
+	struct sockaddr_in sockAddr, sock;
+	int serveur, client, err, nConnect, longaddr , results, resultr;
+	
 
 
-    serveur=socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
-    sockAddr.sin_family=PF_INET;
-    sockAddr.sin_port=htons(2000); 
-    sockAddr.sin_addr.s_addr=inet_addr(arg[1]);
-    addr=sizeof(sockAddr);
+	serveur=socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
+	sockAddr.sin_family=PF_INET;
+	sockAddr.sin_port=htons(2000); 
+	
+	if( argc < 2 )
+	{
+		printf("Working with standard address %s: ", STD_ADDR);
+		sockAddr.sin_addr.s_addr=inet_addr(STD_ADDR);
+	}
+	else
+	{
+		sockAddr.sin_addr.s_addr=inet_addr(arg[1]);
+	}
+	
+	addr=sizeof(sockAddr);
 
-message.label=0.0;
-message.position[0]=0.0;
-message.control[0]=0;
-Te=200000; // Te=100ms
+	message.label=0.0;
+	message.position[0]=0.0;
+	message.control[0]=0;
+	Te=200000; // Te=100ms
 
-fcntl(serveur,F_SETFL,fcntl(serveur,F_GETFL) | O_NONBLOCK); 
+	fcntl(serveur,F_SETFL,fcntl(serveur,F_GETFL) | O_NONBLOCK); 
 
-do{
-usleep(Te);
+	do
+	{
+		usleep(Te);
 
-message.label++;
-results=sendto(serveur,&message,sizeof(message),0,(struct sockaddr*)&sockAddr,sizeof(sockAddr));
-resultr=recvfrom(serveur,&message,sizeof(message), 0,(struct sockaddr*)&sockAddr,&addr);
+		message.label++;
+		results=sendto(serveur,&message,sizeof(message),0,(struct sockaddr*)&sockAddr,sizeof(sockAddr));
+		//resultr=recvfrom(serveur,&message,sizeof(message), 0,(struct sockaddr*)&sockAddr,&addr);
 
-printf("\n client : \n  label=%lf rr=%d rs=%d ",message.label,resultr, results );
+		printf("\n client : \n  label=%lf position=%lf control=%lf rr=%d rs=%d ",message.label,message.position, message.control, resultr, results );
 
-}while(message.label<100.0);
+	} while(message.label<100.0);
 
-close(serveur);
+	close(serveur);
 
-return 0;
+	return 0;
 
 }
 
--- client serveur/server_UDP.c
+++ client serveur/server_UDP.c
@@ -1,75 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <fcntl.h>
+#include "utils.h"
 
-struct mesg {
-double label;
-double position[1];
-double control[1];
+int main (int argc, char *arg[])
+{
 
-};
+	struct mesg message;
+	int result, nsend;
+	struct sockaddr_in sockAddr, sock;
+	int serveur, client, err, nConnect, longaddr;
+	int n , i, results, resultr ;
+	long int  Te;
+	double  Un,  Sn,  Snp , K;
+	double tau, dt, A, B;
+	int Rt;  
+	
 
-#define ERROR (-1)
+	serveur=socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
+	sockAddr.sin_family=PF_INET;
+	sockAddr.sin_port=htons(2000); 
+	sockAddr.sin_addr.s_addr=0;
+	longaddr=sizeof(sockAddr);
 
-int main (int nba, char *arg[]) {
+		err=bind(serveur,(struct sockaddr*)&sockAddr,longaddr);
+	 	if(err==ERROR) {
+		    printf("\n erreur de bind du serveur UDP!! \n");
+		}
 
-struct mesg message;
-int result, nsend;
-struct sockaddr_in sockAddr, sock;
-int serveur, client, err, nConnect, longaddr;
-int n , i, results, resultr ;
-long int  Te;
-double  Un,  Sn,  Snp , K;
-double tau, dt, A, B;
-int Rt;  
-
-  serveur=socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
-    sockAddr.sin_family=PF_INET;
-    sockAddr.sin_port=htons(2000); 
-    sockAddr.sin_addr.s_addr=0;
- 	longaddr=sizeof(sockAddr);
-
-	err=bind(serveur,(struct sockaddr*)&sockAddr,longaddr);
- 	if(err==ERROR) {
-        printf("\n erreur de bind du serveur UDP!! \n");
-	}
-
-message.label=0.0;
-message.position[0]=0.0;
-message.control[0]=0.0;
+	message.label=0.0;
+	message.position[0]=0.0;
+	message.control[0]=0.0;
 
 
-Te=200000; // Te=100ms
+	Te=200000; // Te=100ms
 
-results=ERROR;
-resultr=ERROR;
+	results=ERROR;
+	resultr=ERROR;
 
-fcntl(serveur,F_SETFL,fcntl(serveur,F_GETFL) | O_NONBLOCK); 
- 
-do{
+	fcntl(serveur,F_SETFL,fcntl(serveur,F_GETFL) | O_NONBLOCK); 
+	 
+	do
+	{
 
-usleep(Te); 
+		usleep(Te); 
 
+		resultr=recvfrom(serveur,&message,sizeof(message), 0,(struct sockaddr*)&sockAddr,&longaddr);
 
-resultr=recvfrom(serveur,&message,sizeof(message), 0,(struct sockaddr*)&sockAddr,&longaddr);
-
-printf("server : \n label=%lf rt=%d rr=%d\n",message.label,results,resultr);
-
-
-results=sendto(serveur,&message,sizeof(message),0,(struct sockaddr*)&sockAddr,sizeof(sockAddr));
+		printf("\n server : \n  label=%lf position=%lf control=%lf rr=%d rs=%d ",message.label,message.position, message.control, resultr, results );
 
 
-}while(message.label<100.0);
+		// results=sendto(serveur,&message,sizeof(message),0,(struct sockaddr*)&sockAddr,sizeof(sockAddr));
 
 
-close(serveur);
+	} while(message.label<100.0);
 
-return 0;
+
+	close(serveur);
+
+	return 0;
 
 }
 
